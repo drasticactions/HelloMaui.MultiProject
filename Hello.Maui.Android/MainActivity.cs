2c3bee6bacf37d8f450473e3af82bc3b21abf49a
Tim Miller <innerlogic4321@gmail.com>
2021-04-07 13:27:23 -0400

    Initial Commit

---
+++ Hello.Maui.Android/MainActivity.cs
@@ -0,0 +1,12 @@
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using Microsoft.Maui;
+namespace Hello.Maui.Android
+{
+	[Activity(Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	public class MainActivity : MauiAppCompatActivity
+	{
+	}
+}
---
+++ Hello.Maui.Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using HelloMaui;
+using Microsoft.Maui;
+
+namespace Hello.Maui.Android
+{
+	[Application]
+	public class MainApplication : MauiApplication<Startup>
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership ownerShip) : base(handle, ownerShip)
+		{
+		}
+	}
+}
